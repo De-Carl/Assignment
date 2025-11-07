1693859cd9818835a944e7e503451187b9396c95
Chen Zisheng <chzisheng2024@lzu.edu.cn>
2025-11-07 18:47:25 +0800

    Delete archive directory

--- archive/ai-career-advisor/src/agent/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-智能体模块
-包含职业顾问对话智能体
-"""
-
-from .career_advisor_agent import CareerAdvisorAgent
-
-__all__ = ['CareerAdvisorAgent']
--- archive/ai-career-advisor/src/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-工具模块
-包含可视化和辅助功能
-"""
-
-from .visualization import *
-
-__all__ = ['plot_salary_distribution', 'plot_skill_importance', 'plot_job_categories']
